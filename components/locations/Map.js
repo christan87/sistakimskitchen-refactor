74ee5e31c517ae045d45e52ab94b38ad89e28188
Christan Prices <christanprices@Christans-MacBook-Pro.local>
2024-01-12 07:50:48 -0800

    .env.local

--- components/locations/Map.js
+++ components/locations/Map.js
@@ -2,7 +2,7 @@ import React from "react";
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 const Map = () => {
     
-    let mapsApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+    let mapsApiKey = process.env.GOOGLE_MAPS_API_KEY;
     const mapStyles = {        
         height: "50vh",
         width: "100%"};
@@ -14,7 +14,7 @@ const Map = () => {
   return (
     <div>
       <h1>Locations</h1>
-      <LoadScript googleMapsApiKey={`AIzaSyBO-sm2K0fwQ-kU7y3gSDoDdt1moBhgenE`}>
+      <LoadScript googleMapsApiKey={mapsApiKey}>
          <GoogleMap
            mapContainerStyle={mapStyles}
            zoom={13}
