f91d5a94690b0dac852902a09d8b49267852b2e8
Christan Prices <christanprices@Christans-MacBook-Pro.local>
2024-01-21 04:03:44 -0800

    added the logo as the tab favicon in the public folder as well as included a title

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,8 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap" rel="stylesheet" />
+          <link rel="icon" href="/favicon.ico" />
+          <title>Sista Kim's Kitchen</title>
           {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
           {/* other head elements */}
         </Head>
