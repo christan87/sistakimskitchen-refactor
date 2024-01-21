75f13597f333933155662a263b4ef8d7d0e86b0f
Christan Prices <christanprices@Christans-MacBook-Pro.local>
2024-01-21 04:09:34 -0800

    moved the title to the head in the layout component

--- components/common/Layout.js
+++ components/common/Layout.js
@@ -1,10 +1,15 @@
 import React from "react";  
 import LandingNav from "./LandingNav";
 import Footer from "./Footer";
+import {sistaKimsKitchen} from '../constants';
+
 
 const Layout = ({ children }) => {
     return(
         <div>
+            <head>
+                <title>{sistaKimsKitchen}</title>
+            </head>
             <LandingNav />
             {children}
             <Footer />
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-
 class MyDocument extends Document {
   render() {
     return (
@@ -10,7 +9,6 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap" rel="stylesheet" />
           <link rel="icon" href="/favicon.ico" />
-          <title>Sista Kim's Kitchen</title>
           {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
           {/* other head elements */}
         </Head>
